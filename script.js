091973f3fc556037f1c94eb56223df77043fca91
Ammulu vastupula <148691883+Ammulu-unique@users.noreply.github.com>
2026-05-21 22:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 function insert_Row() {
-    //Write your code here
-	const table=document.getElementById("sampleTable");
-    const row=document.createElement("tr");
-    const cell1=document.createElement("td");
-    const cell2=document.createElement("td");
-    cell1.innerText="New Cell1";
-    cell2.innerText="New Cell2";
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    table.insertBefore(row, table.rows[0]);
-  
+    const table=document.getElementById("sampleTable");
+    const row=table.insertRow(0);
+    const cell1=row.insertCell(0);
+    const cell2=row.insertCell(1);
+    cell1.innerHTML="New Cell1";
+    cell2.innerHTML="New Cell2";
 }
